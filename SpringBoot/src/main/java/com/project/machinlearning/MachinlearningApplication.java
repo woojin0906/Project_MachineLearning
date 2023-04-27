f4267f001cedb674a8805c4be4fab005a0e7c5be
Seungwan Han <hseungwan8@gmail.com>
2023-04-27 15:44:20 +0900

    feat(springboot): 패키지 추가
    스프링부트 패키지 추가

---
+++ SpringBoot/src/main/java/com/project/machinlearning/MachinlearningApplication.java
@@ -0,0 +1,13 @@
+package com.project.machinlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachinlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachinlearningApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/src/test/java/com/project/machinlearning/MachinlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.machinlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachinlearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
