64e395406f24edf07d8292e529be4a0b5bbed18a
Seungwan Han <hseungwan8@gmail.com>
2023-05-21 18:20:48 +0900

    chore(flask): 감정 분류 플라스크 로직 완성

--- flask/app/myapp.py
+++ flask/app/myapp.py
@@ -1,37 +1,287 @@
 from flask import Flask, request, jsonify
-import pickle
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+from kobert_tokenizer import KoBERTTokenizer
+from transformers import BertModel
 
 app = Flask(__name__)
 
-# 피클 파일에서 훈련된 모델을 로드
-with open('simplelinear.pickle', 'rb') as f:
-    model = pickle.load(f)
+class BERTSentenceTransform:
+    r"""BERT style data transformation.
+
+    Parameters
+    ----------
+    tokenizer : BERTTokenizer.
+        Tokenizer for the sentences.
+    max_seq_length : int.
+        Maximum sequence length of the sentences.
+    pad : bool, default True
+        Whether to pad the sentences to maximum length.
+    pair : bool, default True
+        Whether to transform sentences or sentence pairs.
+    """
+
+    def __init__(self, tokenizer, max_seq_length,vocab, pad=True, pair=True):
+        self._tokenizer = tokenizer
+        self._max_seq_length = max_seq_length
+        self._pad = pad
+        self._pair = pair
+        self._vocab = vocab 
+
+    def __call__(self, line):
+        """Perform transformation for sequence pairs or single sequences.
+
+        The transformation is processed in the following steps:
+        - tokenize the input sequences
+        - insert [CLS], [SEP] as necessary
+        - generate type ids to indicate whether a token belongs to the first
+        sequence or the second sequence.
+        - generate valid length
+
+        For sequence pairs, the input is a tuple of 2 strings:
+        text_a, text_b.
+
+        Inputs:
+            text_a: 'is this jacksonville ?'
+            text_b: 'no it is not'
+        Tokenization:
+            text_a: 'is this jack ##son ##ville ?'
+            text_b: 'no it is not .'
+        Processed:
+            tokens: '[CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]'
+            type_ids: 0     0  0    0    0     0       0 0     1  1  1  1   1 1
+            valid_length: 14
+
+        For single sequences, the input is a tuple of single string:
+        text_a.
+
+        Inputs:
+            text_a: 'the dog is hairy .'
+        Tokenization:
+            text_a: 'the dog is hairy .'
+        Processed:
+            text_a: '[CLS] the dog is hairy . [SEP]'
+            type_ids: 0     0   0   0  0     0 0
+            valid_length: 7
+
+        Parameters
+        ----------
+        line: tuple of str
+            Input strings. For sequence pairs, the input is a tuple of 2 strings:
+            (text_a, text_b). For single sequences, the input is a tuple of single
+            string: (text_a,).
+
+        Returns
+        -------
+        np.array: input token ids in 'int32', shape (batch_size, seq_length)
+        np.array: valid length in 'int32', shape (batch_size,)
+        np.array: input token type ids in 'int32', shape (batch_size, seq_length)
+
+        """
+
+        # convert to unicode
+        text_a = line[0]
+        if self._pair:
+            assert len(line) == 2
+            text_b = line[1]
+
+        tokens_a = self._tokenizer.tokenize(text_a) 
+        tokens_b = None
+
+        if self._pair:
+            tokens_b = self._tokenizer(text_b)
+
+        if tokens_b:
+            # Modifies `tokens_a` and `tokens_b` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            self._truncate_seq_pair(tokens_a, tokens_b,
+                                    self._max_seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(tokens_a) > self._max_seq_length - 2:
+                tokens_a = tokens_a[0:(self._max_seq_length - 2)]
+
+        # The embedding vectors for `type=0` and `type=1` were learned during
+        # pre-training and are added to the wordpiece embedding vector
+        # (and position vector). This is not *strictly* necessary since
+        # the [SEP] token unambiguously separates the sequences, but it makes
+        # it easier for the model to learn the concept of sequences.
+
+        # For classification tasks, the first vector (corresponding to [CLS]) is
+        # used as as the "sentence vector". Note that this only makes sense because
+        # the entire model is fine-tuned.
+        #vocab = self._tokenizer.vocab
+        vocab = self._vocab
+        tokens = []
+        # tokens.append(vocab.cls_token)
+        tokens.append('[CLS]')
+        tokens.extend(tokens_a)
+        # tokens.append(vocab.sep_token)
+        tokens.append('[SEP]')
+        segment_ids = [0] * len(tokens)
+
+        if tokens_b:
+            tokens.extend(tokens_b)
+            # tokens.append(vocab.sep_token)
+            tokens.append('[SEP]')
+            segment_ids.extend([1] * (len(tokens) - len(segment_ids)))
+
+        input_ids = self._tokenizer.convert_tokens_to_ids(tokens)
+
+        # The valid length of sentences. Only real  tokens are attended to.
+        valid_length = len(input_ids)
+
+        if self._pad:
+            # Zero-pad up to the sequence length.
+            padding_length = self._max_seq_length - valid_length
+            # use padding tokens for the rest
+            input_ids.extend([1] * padding_length)
+
+            segment_ids.extend([0] * padding_length)
+
+        return np.array(input_ids, dtype='int32'), np.array(valid_length, dtype='int32'),\
+            np.array(segment_ids, dtype='int32')
+    
+# ★
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, vocab, max_len,
+                 pad, pair):
+        transform = BERTSentenceTransform(bert_tokenizer, max_seq_length=max_len,vocab=vocab, pad=pad, pair=pair)
+        #transform = nlp.data.BERTSentenceTransform(
+        #    tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
 
 
-@app.route('/') #루트
-def index():
-    hi = 'hello world'
-    return hi
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=7,   ##클래스 수 조정##
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
 
-@app.route("/sign", methods=['POST']) #포스트 방식 테스트
-def sign():
-    temp = request.form.get("user")
-    return temp
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device),return_dict=False)
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
 
-# 간단한 선형 회귀 모델을 통한 api
-@app.route('/predict', methods=['POST'])
-def predict():
-    # 요청 데이터
-    data = request.get_json(force=True)
+#model = torch.load('app/7emotions_model.pt', map_location=torch.device('cpu'))  # 전체 모델을 통째로 불러옴, 클래스 선언 필수
 
-    # 입력 값이 단일 숫자인 경우 => 배열로 변환
-    input_data = [data['input']] if isinstance(data['input'], (int, float)) else data['input']
+device = torch.device("cpu")
+bertmodel = BertModel.from_pretrained('skt/kobert-base-v1', return_dict=False)
+model = BERTClassifier(bertmodel,  dr_rate=0.5).to(device)
+model.load_state_dict(torch.load('app/7emotions_model_state_dict.pt', map_location='cpu'))
 
-    # 예측 수행
-    prediction = model.predict([input_data])[0]
+# Setting parameters
+max_len = 64
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate =  5e-5
 
-    # JSON 반환
-    return jsonify({'output': prediction})
+#optimizer와 schedule 설정
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+
+
+tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+# vocab = nlp.vocab.BERTVocab.from_sentencepiece(tokenizer.vocab_file, padding_token='[PAD]')
+vocab = 'vocab.txt'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+def predict(predict_sentence):
+
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tokenizer, vocab, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=0)
+    
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length= valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+        test_eval=[]
+        for i in out:
+            logits=i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("공포가")
+            elif np.argmax(logits) == 1:
+                test_eval.append("놀람이")
+            elif np.argmax(logits) == 2:
+                test_eval.append("분노가")
+            elif np.argmax(logits) == 3:
+                test_eval.append("슬픔이")
+            elif np.argmax(logits) == 4:
+                test_eval.append("중립이")
+            elif np.argmax(logits) == 5:
+                test_eval.append("행복이")
+            elif np.argmax(logits) == 6:
+                test_eval.append("혐오가")
+
+        return(">> 입력하신 내용에서 " + test_eval[0] + " 느껴집니다.")
+
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    data = request.get_json()  # 클라이언트로부터 데이터를 받아옴
+    text = data['text']  # 예측을 위한 텍스트 데이터 추출
+
+    # predict 메서드를 호출하여 예측 수행
+    result = predict(text)
+
+    # 예측 결과를 JSON 형식으로 반환
+    response = {'해당 글의 감정': result}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
